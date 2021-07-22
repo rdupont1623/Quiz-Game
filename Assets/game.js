c5d834e1267913b7c7fdc433de7d297df1746661
Hazelton Chrome <rdupont1623@gmail.com>
2021-07-22 12:13:51 -0400

    Pushing up orgiginal foundation of the css and html. Also started up on the JS

---
+++ Assets/game.js
@@ -0,0 +1,8 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelector('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFul');
+
+
+let currentQuestion = {}
