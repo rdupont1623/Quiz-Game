4584f9d09eea1f4d632a9ed86dfa1e667054310e
Hazelton Chrome <rdupont1623@gmail.com>
2021-07-25 14:16:25 -0400

    Pushing up the majority of this project, I've been making changes on it now for a week

--- Assets/game.js
+++ Assets/game.js
@@ -1,8 +1,132 @@
+/*Initializing J Queries- connecting ID's to their
+ corresponding JS values*/ 
+
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelector('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('#progressBarFul');
+const progressBarFull = document.querySelector('#progressBarFull');
+const count = document.querySelector('#count');
 
+/*Initializing initial values for variables*/ 
 
 let currentQuestion = {}
+let availableQuestions = []
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+/* establishing our questions => The questions are broken down into choices. The answer to that question is the value of the correct choice. */ 
+
+
+let questions = [
+    {
+        question: "What is 2 + 2?",
+        choice1:'2',    
+        choice2:'4',        
+        choice3:'21',        
+        choice4:'17',     
+        answer: 2,   
+    },
+    {
+        question:'The tallest building in the world is located in which city?',
+        choice1:'Dubai',    
+        choice2:'New York',        
+        choice3:'Shanhai',        
+        choice4:'None of the above ',     
+        answer: 1,   
+    },
+    {
+        question:'What percent of American adults believe that chocolate milk comes from brown cows?',
+        choice1:'20%',    
+        choice2:'18%',        
+        choice3:'7%',        
+        choice4:'33%',     
+        answer: 3,   
+    },
+    {
+        question: 'Approximately what percent of U.S. power outages are caused by squirrels?',
+        choice1:'10-20%',    
+        choice2:'5-10%',        
+        choice3:'15-20%',        
+        choice4:'30-40%',     
+        answer: 1,   
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion ()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+/* The choices function takes in the array variable, which has been queried as an array  */
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+        'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+        })
+
+    })
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+let interval = setInterval(function (){
+    count.innertext='${count}';
+    count--;
+    if (count === 0) {
+        clearInterval(interval);
+        alert("You're out of time")
+        let count = 15;
+    }
+}, 1000);
+
+startGame()
